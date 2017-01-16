ca3866ecdf96f8729f935a31f58d5dbbc8103a2e
Michael McCune <msm@redhat.com>
2017-01-16 17:58:48 -0500

    remove hello world stuff
    This change starts to move towards the static content representation.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+class VaRApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Enter stock tickers to predict</h1>
+          <input type="text"></input>
+          <button>Add</button><br />
+          <button>Submit</button>
+        </div>
+        <div>
+          <h1>Results</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world</h1>,
+  <VaRApp />,
   document.getElementById('root')
 );
