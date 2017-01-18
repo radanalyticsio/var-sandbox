3dcf8e2a8a01606bbd1a2ce4352208b418aaa47f
Michael McCune <msm@redhat.com>
2017-01-18 16:57:20 -0500

    change devel command to use spark-submit
    As the application moves closer to requiring spark, this change will
    make sure that the devel and production targets are closer.

--- scripts/devel.sh
+++ scripts/devel.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+if [ -z "$SPARK_SUBMIT_CMD" ]
+then
+    echo "Error: please tell me where the spark-submit command is by setting SPARK_SUBMIT_CMD"
+    exit 1
+fi
+
 pushd dist
 export LC_ALL=en_US.utf8
 export LANG=en_US.utf8
-python app.py
+$SPARK_SUBMIT_CMD app.py
 popd
 
